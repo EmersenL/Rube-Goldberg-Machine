09602cc340069b2779f26120e064eb8826cae3e6
mperina11 <mperina173@gmail.com>
2022-11-28 14:55:03 -0800

    Stairs and Dominos

--- Assets/mov.cs
+++ Assets/mov.cs
@@ -5,18 +5,25 @@ using UnityEngine;
 public class mov : MonoBehaviour
 {
     [SerializeField]
-    private float x = 0.00001f;
+    private Rigidbody rb;
+
+    [SerializeField]
+    private float x = 0.0001f;
+
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-            transform.position = new Vector3(transform.position.x + x, transform.position.y, transform.position.z);
+        
+        transform.position = new Vector3(transform.position.x + x, transform.position.y, transform.position.z);
+        rb.velocity = Vector3.zero;
+        rb.isKinematic = true;
      
     }
 }
